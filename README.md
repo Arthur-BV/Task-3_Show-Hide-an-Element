096a82ed8d35e738549af4da25765ff733174523
Arthur V. Babayan <arthur.babayan@gmail.com>
2024-02-23 23:16:11 +0400

    Added task files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Task-3_Show-Hide-an-Element
---
+++ index.js
@@ -0,0 +1,15 @@
+var toggle = true;
+const textArea = document.querySelector("#textArea");
+const toggleButton = document.querySelector("#toggleButton");
+
+toggleButton.addEventListener("click", function () {
+    if (toggle) {
+        toggleButton.textContent = "Show";
+        textArea.style.display = "none";
+        toggle = false;
+    } else {
+        toggleButton.textContent = "Hide";
+        textArea.style.display = "block";
+        toggle = true;
+    }
+});
